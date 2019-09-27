7c9028ad98991d2cfd4ee50651567a8e5cca3a89
golubenkonick <golubenkonick@gmail.com>
2019-09-27 19:45:16 +0300

    Scaling Fix

--- WindowsFormsGraphics_math/Label.cs
+++ WindowsFormsGraphics_math/Label.cs
@@ -7,9 +7,13 @@ using System.Drawing;
 namespace Geogebra3
 {
     class Label  // point has a label == композиция
-    {     
-        public double offsetX = 0.3; // distance between point and label in metres
-        public double offsetY = -0.3; // distance between point and label in metres
+    {
+        // ++++ 1 способ: offsetX = 5; // distance between point and label in pixels and is constant while scaling
+        // 2 способ: offsetX = 0.3; // distance between point and label in metres and is changing while scaling  -- bad 
+
+
+        public int offsetX = 75; // distance between point and label in pixels
+        public int offsetY = -75; // distance between point and label in pixels
         string name;
         bool visible;
         public static char lastLabel = 'A';      
@@ -38,11 +42,11 @@ namespace Geogebra3
         {
             if (visible)
             {
-                int labelX = cs.RealToVisualX(x + this.offsetX); 
-                int labelY = cs.RealToVisualY(y + this.offsetY); 
+                int labelX = cs.RealToVisualX(x) + this.offsetX; 
+                int labelY = cs.RealToVisualY(y) + this.offsetY; 
                 SizeF size = g.MeasureString(name, cs.fontLabel);
-                this.height = cs.VisualToRealDistance(size.Height);
-                this.width = cs.VisualToRealDistance(size.Width);
+                this.height = size.Height;
+                this.width = size.Width;
                 // g.DrawRectangle(Pens.Red, labelX , labelY, size.Width, size.Height);
                 String labelName = name + "(" + Math.Round(x, 1) + "; " + Math.Round(y, 1) + ")";
                 g.DrawString(labelName, cs.fontLabel, cs.brush, labelX, labelY);
